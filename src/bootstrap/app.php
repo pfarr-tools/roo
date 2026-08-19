9ddd91fc7f7beab32ff09ac79c74d269d742a5bd
Christoph Fischer <chris@toph.de>
2026-08-19 12:32:40 +0200

    fix(inertia): Flashdaten für Planungsbestätigungen teilen

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\HandleInertiaRequests;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -14,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         // Trust the host-level TLS reverse proxy so Laravel detects HTTPS.
         $middleware->trustProxies(at: '*');
+        $middleware->web(append: [HandleInertiaRequests::class]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
